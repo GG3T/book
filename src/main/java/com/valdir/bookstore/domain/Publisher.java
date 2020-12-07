b868da53be41fd2e949cb29859e3ad374ebec4a6
Valdir Cezar <valdircezar312@gmail.com>
2020-12-07 07:56:25 -0300

    Publisher domain class

---
+++ src/main/java/com/valdir/bookstore/domain/Publisher.java
@@ -0,0 +1,89 @@
+package com.valdir.bookstore.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Publisher {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Integer code;
+	private Date foundation_date;
+
+	public Publisher() {
+		super();
+	}
+
+	public Publisher(Integer id, String name, Integer code, Date foundation_date) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.code = code;
+		this.foundation_date = foundation_date;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+
+	public Date getFoundation_date() {
+		return foundation_date;
+	}
+
+	public void setFoundation_date(Date foundation_date) {
+		this.foundation_date = foundation_date;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Publisher other = (Publisher) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
